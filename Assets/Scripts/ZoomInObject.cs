89faf8976d4ce7bcff52884ab8e400149683726b
Rafeljody <rafaeljody@gmail.com>
2020-06-20 01:28:15 +0700

    inisialisasi TG Unlock The Door

---
+++ Assets/Scripts/ZoomInObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomInObject : MonoBehaviour, IInteractable
+{
+
+    public float ZoomRatio = .5f;
+
+
+    public void Interact(DisplayImage currentDisplay)
+    {
+        Camera.main.orthographicSize *= ZoomRatio;
+        Camera.main.transform.position = new Vector3(this.transform.position.x, this.transform.position.y,
+            Camera.main.transform.position.z);
+    }
+}
